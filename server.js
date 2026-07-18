af92823be1255d512291f8d509883ec65f928fe8
johnella-stack <johnelladon3@gmail.com>
2026-07-18 21:48:17 +0800

    use gmail smtp

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ dotenv.config()
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 })
 console.log('MYSQLHOST:', process.env.MYSQLHOST)
